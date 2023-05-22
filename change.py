02c3b1d910ca3eea093f5b9810fd42631d3e935a
Milco Numan <milco.numan@gmail.com>
2023-05-22 13:40:33 +0200

    Removed return values

--- change.py
+++ change.py
@@ -3,9 +3,9 @@ COINS = [1, 5, 10, 25, 50]
 
 
 def count_change(change: int, coins: list[int]) -> int:
-    # succes als change = 0 -> 1
-    # onmogelijk als change < 0 of coins = [] -> 0
-    # doorgaan zonder/gebruik eerste muntsoort -> recursie
+    # succes als change = 0
+    # onmogelijk als change < 0 of coins = []
+    # doorgaan zonder/gebruik eerste muntsoort
     pass
 
 
